9987bb14eb578d20b2f5088706848f79dd4cdee5
Truemel <tgmelipilla@gmail.com>
2023-04-12 18:22:46 -0400

    Mejoras de funcionamiento

--- calculadora.js
+++ calculadora.js
@@ -1,8 +1,114 @@
 let numero1 = '';
 let numero2 = '';
-let operacion = '';
+let operacion = '0';
 let resultado = '';
 
+var valorAct = 0;
+var chain = true;
+
+$(document).ready(()=>{
+  $(".op").prop("disabled", true);
+  $("#resultado").val(0);
+});
+
+function agregarNumero(numero) {
+  if(!chain)
+    document.getElementById("resultado").value = $('#resultado').val()+numero;
+  else{
+    document.getElementById("resultado").value = numero;
+    chain = false;
+  }
+  $(".op").prop("disabled", false);
+  $(".equ").prop("disabled", false);
+}
+
+function agregarDecimal(){
+  if($("#resultado").val() !== ""){
+    $("#resultado").val($("#resultado").val()+".");
+    $(".op").prop("disabled", true);
+    $(".dec").prop("disabled", true);
+    $(".equ").prop("disabled", true);
+    chain = false;
+  }
+}
+
+function agregarOperacion(op) {
+  $(".dec").prop("disabled", false);
+  var valOper = parseFloat($("#resultado").val());
+  if (operacion === '+') {
+    valorAct += valOper;
+  } else if (operacion === '-') {
+    valorAct -= valOper;
+  } else if (operacion === '*') {
+    valorAct *= valOper;
+  } else if (operacion === '/') {
+    if ($("#resultado").val() === '0') {
+        alert('No se puede dividir por cero');
+        return;
+    }
+    valorAct /= valOper;
+  }else if(operacion === "%")
+    valorAct %= valOper;
+  else if(operacion === "^")
+    valorAct = poten(valorAct, valOper);
+  
+  if(operacion !== "0"){
+    $("#resultado").val(valorAct);
+    chain = true;
+  }else{
+    valorAct = parseFloat($("#resultado").val());
+    $("#resultado").val(0);
+    chain = true;
+  }
+   $(".op").prop("disabled", true);
+   operacion = op;
+}
+
+function poten(val1, val2){
+  if(val2 == 0)
+    return 1;
+  else
+    return val1*poten(val1, val2-1);
+}
+
+function raiz() {
+  if (parseFloat($("#resultado").val()) < 0) {
+      alert('número imaginario, valor debe ser igual o mayor a 0');
+      return;
+  }
+  valorAct = Math.sqrt(parseFloat($("#resultado").val()));
+  $("#resultado").val(valorAct);
+  operacion = "0";
+  chain = true;
+  $(".dec").prop("disabled", false);
+}
+
+function calcular(){
+  $(".dec").prop("disabled", false);
+  var valOper = parseFloat($("#resultado").val());
+  if (operacion === '+') {
+    valorAct += valOper;
+  } else if (operacion === '-') {
+    valorAct -= valOper;
+  } else if (operacion === '*') {
+    valorAct *= valOper;
+  } else if (operacion === '/') {
+    if ($("#resultado").val() === '0') {
+        alert('No se puede dividir por cero');
+        return;
+    }
+    valorAct /= valOper;
+  }else if(operacion === "%")
+    valorAct %= valOper;
+  else if(operacion === "^")
+    valorAct = poten(valorAct, valOper);
+    
+  $("#resultado").val(valorAct);
+  chain = true;
+  operacion = "0";
+}
+
+/*
 function agregarNumero(numero) {
     if (operacion === '') {
         numero1 += numero;
@@ -11,18 +117,25 @@ function agregarNumero(numero) {
     }
     actualizarResultado();
 }
-
+*/
+/*
 function agregarOperacion(op) {
     operacion = op;
     actualizarResultado();
 }
-
+*/
 function borrar() {
     numero1 = '';
     numero2 = '';
-    operacion = '';
+    operacion = '0';
     resultado = '';
-    actualizarResultado();
+    valorAct = 0;
+    chain = true;
+    $(".op").prop("disabled", true);
+    $(".dec").prop("disabled", false);
+    $(".equ").prop("disabled", false);
+    $("#resultado").val(0);
+    //actualizarResultado();
 }
 
 function actualizarResultado() {
@@ -49,7 +162,7 @@ function validarNumeros() {
     }
     return true;
 }
-
+/*
 function calcular() {
     if (!validarNumeros()) {
         return;
@@ -75,11 +188,14 @@ function calcular() {
     operacion = '';
     actualizarResultado();
 }
+*/
 function calcularPorcentaje() {
     let pantalla = document.getElementById("pantalla");
     let resultado = eval(pantalla.value);
     pantalla.value = resultado/100;
   }
+
+  /*
 function raiz() {
     if (numero1 === '') {
         alert('Por favor ingresa un número');
@@ -90,6 +206,7 @@ function raiz() {
     numero1 = res;
     actualizarResultado();
 }
+*/
 
 function potencia() {
     if (numero1 === '') {
